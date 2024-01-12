6ee911efb2713b823fa73eadf1a5bbbe9e7494e2
Rangha-Vardhan-1552 <rangavardhang1908@gmail.com>
2024-01-13 02:34:48 +0530

    create react app

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold my-7  text-center text-slate-700">
+      Welcome to SaaS Application... @TensorGo
+    </h1>
+  )
+}
